80474fa9fab12d8bbb8bd6fff25e1e232a95a826
xifoxy-ru <xifoxy.ru@gmail.com>
2026-04-07 20:24:01 +0900

    change name

--- 04_bonus/cls_bonus.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-BASE="$(cd "$(dirname "$0")" && pwd)"
-
-rm -f bonus_demo_log
-docker comopsoe down -v >/dev/null 2>&1
-docker rm -f $(docker ps -aq) >/dev/null 2>&1
-docker rmi -f $(docker images -q) >/dev/null 2>&1
